ccdee8f091e18dee25e16bdcd35276f3e346450f
@dianacres <dianacres@gmail.com>
2022-10-07 16:10:08 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
 import './App.css';
+import Titulo from './message';
 
-function App() {
+
+
+
+const Descripcion = () => {
+  return <p>App curso fullstack</p>
+}
+
+const App = () => {
+
+  const a = 2
+  const b = 3  
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App"> 
+    <Titulo color = 'red' message = 'Estamos en un curso de React'/>
+    <Descripcion/>
+      <div>
+        <p>el resultado es: </p>
+        {a+b}
+      </div>
+      
     </div>
   );
 }
